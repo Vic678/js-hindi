c3f0d2eee3991db5c2ca9ca542fe2e73705bae28
Vic678 <84173599+Vic678@users.noreply.github.com>
2023-08-08 14:40:18 +0000

    lec1

---
+++ Bsics/lec1.js
@@ -0,0 +1,8 @@
+const accountID = 144532
+let accountEmail = "vicky@gmail.com"
+var accountpswd = "12345"
+accountcity = "Delhi"
+let accountState;
+accountcity ="VARANASI"
+console.log(accountID);
+console.table([accountID, accountEmail, accountpswd, accountcity, accountState])
